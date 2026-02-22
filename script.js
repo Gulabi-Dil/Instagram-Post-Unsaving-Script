0d4b05f3f57de430a0e3dd3745f97ec2d347f3f6
Gulabi-Dil <thegreatmindhehe@gmail.com>
2026-02-22 19:07:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+function getCookie(name) {
+    return document.cookie.split('; ').find(r=>r.startsWith(name+'=')).split('=')[1];
+}
+
+const headers = {
+    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0',
+    'X-IG-App-ID': '936619743392459',
+    'X-CSRFToken':getCookie('csrftoken'),
+}
+
 async function unsaveAllPosts() {
     let totalUnsaved = 0;
-    let nextId = null;
+    let nextMaxId = null;
     while (true) {
-        const url = nextId
-            ? `/api/v1/feed/saved/posts/?max_id=${nextId}`
+        const url = nextMaxId
+            ? `/api/v1/feed/saved/posts/?max_id=${nextMaxId}`
             : `/api/v1/feed/saved/posts/`;
 
         const res = await fetch(url, { headers, credentials: 'include' })
